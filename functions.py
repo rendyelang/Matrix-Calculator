6171dd11cc61131b204c2760f6eedd839fcabb74
Baraa <baraa.s.alsabri@gmail.com>
2024-03-09 19:13:13 +0700

    Checking matrices dimensions logic added

--- functions.py
+++ functions.py
@@ -19,14 +19,4 @@ def subtract_matrices(matrix1 , matrix2):
 
     return result
 
-def check_multiplication_validity(matrix_1 , matrix_2):
-    # The rule is: the number of columns in the first matrix must be equal to the number of rows in the second matrix.
-    number_of_columns_first_matrix = len(matrix_1[0]) # E.g. if matrix_1 = [[1,2] , [3,4] , [5,6]], matrix_1[0] will be [1,2] so len(matrix_1) will be 2, which is the number of columns.
-    number_of_rows_second_matrix = len(matrix_2) # E.g. if matrix_2 = [[1,2] , [3,4] , [5,6]], len(matrix_2) will be 3, which is the number of rows.
-    
-    if number_of_columns_first_matrix == number_of_rows_second_matrix:
-        is_valid = True
-    else:
-        is_valid = False
-    
-    return is_valid
+
--- main.py
+++ main.py
@@ -4,30 +4,6 @@ print("========Welcome to Matrix Calculator========")
 print("====Programed by Baraa, Rendy, and Ahmed====")
 print()
 
-number_of_rows = int(input(f"Enter the number of rows for the matrices : "))
-number_of_columns = int(input(f"Enter the number of columns for the matrices : "))
-
-matrix_1 = []
-for row in range(number_of_rows):
-    row_values = []
-    for column in range(number_of_columns):
-        value = int(input(f"Enter a value for the first matrix posation {row + 1},{column + 1}: " )) # We add one because the count in python starts from zero
-        row_values.append(value)
-    matrix_1.append(row_values)
-    # At the end matrix_1 will look something like this [[5,4] , [6,8] , [9,4]]
-
-matrix_2 = []
-for row in range(number_of_rows):
-    row_values = []
-    for column in range(number_of_columns):
-        value = int(input(f"Enter a value for the second matrix posation {row + 1},{column + 1}: " )) # We add one because the count in python starts from zero
-        row_values.append(value)
-    matrix_2.append(row_values)
-    # At the end matrix_1 will look something like this [[5,4] , [6,8] , [9,4]]
-
-# print(matrix_1)
-# print(matrix_2)
-    
 
 def menu():
     print("Menu:")
@@ -40,19 +16,91 @@ def menu():
     print()
     if chose_menu == 1:
         print("You chose to add matrices")
+        number_of_rows = int(input(f"Enter the number of rows for the matrices : "))
+        number_of_columns = int(input(f"Enter the number of columns for the matrices : "))
+
+        matrix_1 = []
+        for row in range(number_of_rows):
+            row_values = []
+            for column in range(number_of_columns):
+                value = int(input(f"Enter a value for the first matrix posation {row + 1},{column + 1}: " )) # We add one because the count in python starts from zero
+                row_values.append(value)
+            matrix_1.append(row_values)
+            # At the end matrix_1 will look something like this [[5,4] , [6,8] , [9,4]]
+
+        matrix_2 = []
+        for row in range(number_of_rows):
+            row_values = []
+            for column in range(number_of_columns):
+                value = int(input(f"Enter a value for the second matrix posation {row + 1},{column + 1}: " )) # We add one because the count in python starts from zero
+                row_values.append(value)
+            matrix_2.append(row_values)
+            # At the end matrix_1 will look something like this [[5,4] , [6,8] , [9,4]]
+
+
         # call function add_matrices here
 
     elif chose_menu == 2:
         print("You chose to subtract matrices")
+        number_of_rows = int(input(f"Enter the number of rows for the matrices : "))
+        number_of_columns = int(input(f"Enter the number of columns for the matrices : "))
+
+        matrix_1 = []
+        for row in range(number_of_rows):
+            row_values = []
+            for column in range(number_of_columns):
+                value = int(input(f"Enter a value for the first matrix posation {row + 1},{column + 1}: " )) # We add one because the count in python starts from zero
+                row_values.append(value)
+            matrix_1.append(row_values)
+            # At the end matrix_1 will look something like this [[5,4] , [6,8] , [9,4]]
+
+        matrix_2 = []
+        for row in range(number_of_rows):
+            row_values = []
+            for column in range(number_of_columns):
+                value = int(input(f"Enter a value for the second matrix posation {row + 1},{column + 1}: " )) # We add one because the count in python starts from zero
+                row_values.append(value)
+            matrix_2.append(row_values)
+            # At the end matrix_1 will look something like this [[5,4] , [6,8] , [9,4]]
+
         # call function subtract_matrices here
 
     elif chose_menu == 3:
         print("You chose to multiply matrices")
-        is_valid = check_matrices_length(matrix_1 , matrix_2) # To check if the number of columns in the first matrix must be equal to the number of rows in the second matrix.
-        if not is_valid:
-            print('In order to muliply two matrices, the number of columns in the first matrix must be equal to the number of rows in the second matrix.')
-        else:
+        number_of_rows_matrix_1 = int(input(f"Enter the number of rows for the first matrix : "))
+        number_of_columns_matrix_1 = int(input(f"Enter the number of columns for the first matrix : "))
+  
+        number_of_rows_matrix_2 = int(input(f"Enter the number of rows for the second matrix : "))
+        number_of_columns_matrix_2 = int(input(f"Enter the number of columns for the second matrix : "))
+
+
+        matrix_1 = []
+        for row in range(number_of_rows_matrix_1):
+            row_values = []
+            for column in range(number_of_columns_matrix_1):
+                value = int(input(f"Enter a value for the first matrix posation {row + 1},{column + 1}: " )) # We add one because the count in python starts from zero
+                row_values.append(value)
+            matrix_1.append(row_values)
+            # At the end matrix_1 will look something like this [[5,4] , [6,8] , [9,4]]
+
+        matrix_2 = []
+        for row in range(number_of_rows_matrix_2):
+            row_values = []
+            for column in range(number_of_columns_matrix_2):
+                value = int(input(f"Enter a value for the second matrix posation {row + 1},{column + 1}: " )) # We add one because the count in python starts from zero
+                row_values.append(value)
+            matrix_2.append(row_values)
+            # At the end matrix_1 will look something like this [[5,4] , [6,8]]
+        
+        if number_of_columns_matrix_1 == number_of_rows_matrix_2: # Check if the number of columns in the first matrix must be equal to the number of rows in the second matrix.
+            # Call the multiplcation function
             result = multiply_matrices(matrix_1 , matrix_2)
+            print('The multiplication result is: ')
+            for row in result:
+                print(row)
+        else:
+            print("The number of columns in the first matrix must be equal to the number of rows in the second matrix.")
+
 
     else:
         print("You typed the wrong number")
