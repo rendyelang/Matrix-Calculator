ca5146b70ed369e3e17c36dffa9d443760dccd80
Ahmed Baghni <161574715+Ahmedbaghni@users.noreply.github.com>
2024-03-10 17:36:14 +0700

    Add files via upload
    Subtraction operator added

---
+++ functionSUB.py
@@ -0,0 +1,29 @@
+def add_matrices(matrix1, matrix2):
+    result = []
+    # By Rendy
+    for i in range(len(matrix1)):
+        row = []
+        for j in range(len(matrix1[0])):
+            row.append(matrix1[i][j] + matrix2[i][j])
+        result.append(row)
+
+    return result
+
+def multiply_matrices(matrix1 , matrix2):
+    result = []
+    # By Baraa
+
+
+    return result
+
+def subtract_matrices(matrix1 , matrix2):
+    result = []
+    for i in range(len(matrix1)):
+        row = []
+        for j in range(len(matrix1[0])):
+            row.append(matrix1[i][j] - matrix2[i][j])
+        result.append(row)
+
+    # By Ahmed
+
+    return result
