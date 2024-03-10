ac4ee710aa7f52e2859d2dae2c36d6a607baac41
Baraa <baraa.s.alsabri@gmail.com>
2024-03-10 19:02:15 +0700

    Addational file removed

--- functions.py
+++ functions.py
@@ -17,7 +17,13 @@ def multiply_matrices(matrix1 , matrix2):
     return result
 
 def subtract_matrices(matrix1 , matrix2):
-    result = []
     # By Ahmed
+    result = []
+    for i in range(len(matrix1)):
+        row = []
+        for j in range(len(matrix1[0])):
+            row.append(matrix1[i][j] - matrix2[i][j])
+        result.append(row)
+
 
     return result
